869df17e5ccb88f16d9f866d08509ffc124ea30b
Shawn Hsu <ambersun1019.shawn@gmail.com>
2024-11-02 22:46:00 +0800

    Fix local path reference check

--- scripts/link.sh
+++ scripts/link.sh
@@ -18,6 +18,8 @@ for file in $(find ${root} -name "*.md"); do
         # remove the trailing # and the following characters
         fullpath=$(echo ${match} | sed 's/[()]//g' | sed 's/]//g' | sed 's/%20/-/g')
 
+        # remove /# and any character behind it with empty string
+        fullpath=$(echo ${fullpath} | sed 's/\/#.*//')
         filenameWithAnchor=$(basename ${fullpath})
 
         # remove anchor
@@ -25,7 +27,7 @@ for file in $(find ${root} -name "*.md"); do
         dirname=$(dirname ${fullpath})
 
         testPath="${dummy}/${dirname}/*-${filename}.md"
-        
+
         # check if the file exists
         if ! ls ${testPath} 1> /dev/null 2>&1; then
             echo -e "\tFile not found: ${fullpath}"
